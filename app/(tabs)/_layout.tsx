182a562ebb767fd989d827b3aa2e768e208869d1
Fortune Zviregei <fortunechainz@gmail.com>
2024-03-17 01:45:27 +0100

    Add typedRoutes and tsconfigPaths to app.json, and create new files for tabs and their respective components.

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Tabs } from 'expo-router'
+import Colors from '@/constants/Colors'
+import { FontAwesome5, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
+
+const Layout = () => {
+  return (
+    <Tabs screenOptions={{
+      tabBarActiveTintColor: Colors.primary,
+      tabBarLabelStyle: {
+        fontFamily: "mono-sb"
+      }
+    }}>
+      <Tabs.Screen name="index" options={{
+        tabBarLabel: "Explore", tabBarIcon: ({ color, size }) => <Ionicons name="search" color={color} size={size} />
+      }}></Tabs.Screen>
+      <Tabs.Screen name="wishlists" options={{
+        tabBarLabel: "Wishlists", tabBarIcon: ({ color, size }) => <Ionicons name="heart-outline" color={color} size={size} />
+      }}></Tabs.Screen>
+      <Tabs.Screen name="trips" options={{
+        tabBarLabel: "Trips", tabBarIcon: ({ color, size }) => <FontAwesome5 name="airbnb" color={color} size={size} />
+      }}></Tabs.Screen>
+      <Tabs.Screen name="inbox" options={{
+        tabBarLabel: "Inbox", tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="message-outline" color={color} size={size} />
+      }}></Tabs.Screen>
+      <Tabs.Screen name="profile" options={{
+        tabBarLabel: "Profile", tabBarIcon: ({ color, size }) => <Ionicons name="person-outline" color={color} size={size} />
+      }}></Tabs.Screen>
+    </Tabs>
+  )
+}
+
+export default Layout
